256b5b401848fbb11f71c7ce53b1d128a4ca0855
Piyush Panchal <piyush.panchal@sparkventures.co.nz>
2017-02-10 08:54:25 +1300

    Added selenide to open google

--- regression/src/main/java/nz/co/automation/regression/steps/GoogleSteps.java
+++ regression/src/main/java/nz/co/automation/regression/steps/GoogleSteps.java
@@ -4,9 +4,13 @@ import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
 
+import static com.codeborne.selenide.Selenide.open;
+
 public class GoogleSteps {
     @Given("^I am google page$")
     public void iAmGooglePage() {
+        //GooglePage page = open("http://google.com/ncr", GooglePage.class);
+        open("http://www.google.co.nz");
     }
 
     @Given("^I have a \"([^\"]*)\" search query$")
