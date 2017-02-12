faf86ed750c166ad0be0c575e8d3d7cb564eff4c
Nick Tran <nick.tran@sparkventures.co.nz>
2017-02-13 09:47:25 +1300

    Added AutomationConfiguration so we can run steps as a TestSuite

---
+++ regression/src/main/java/nz/co/automation/regression/AutomationConfiguration.java
@@ -0,0 +1,14 @@
+package nz.co.automation.regression;
+
+import cucumber.runtime.java.spring.CucumberGlueScopeConfiguration;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Import;
+
+@ComponentScan({"nz.co.automation"})
+@Import(CucumberGlueScopeConfiguration.class)
+@EnableAutoConfiguration
+public class AutomationConfiguration extends SpringBootServletInitializer {
+
+}
--- regression/src/main/java/nz/co/automation/regression/steps/GoogleSteps.java
+++ regression/src/main/java/nz/co/automation/regression/steps/GoogleSteps.java
@@ -3,24 +3,18 @@ package nz.co.automation.regression.steps;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
+import nz.co.automation.regression.AutomationConfiguration;
 import nz.co.automation.regression.pages.Browser;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.SpringBootConfiguration;
 import org.springframework.boot.test.context.SpringBootContextLoader;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.FilterType;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@SpringBootConfiguration
-@ContextConfiguration(loader = SpringBootContextLoader.class)
+@ContextConfiguration(classes = AutomationConfiguration.class, loader = SpringBootContextLoader.class)
 @SpringBootTest
-@ComponentScan(basePackages = {"nz.co.automation", "cucumber.runtime"}, excludeFilters = {
-        // We must exclude the following configuration classes because they don't work with the Bigpipe Services setup
-        @ComponentScan.Filter(type = FilterType.REGEX, pattern = ".*\\.*Steps.*")})
 public class GoogleSteps {
 
     private final Browser browser;
