ed2b3f39fc9904282a105bd0ce39a2004075c7f6
Edward Tandjung <edward.tandjung@sparkventures.co.mz>
2017-02-15 08:29:06 +1300

    Added new banner - created base steps

--- regression/src/main/java/nz/co/automation/regression/steps/GoogleSteps.java
+++ regression/src/main/java/nz/co/automation/regression/steps/GoogleSteps.java
@@ -12,10 +12,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
-@RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(classes = AutomationConfiguration.class, loader = SpringBootContextLoader.class)
-@SpringBootTest
-public class GoogleSteps {
+
+public class GoogleSteps extends BaseSteps {
 
     private final Browser browser;
 
