0640cca2b6d736f9a194a05ffeaf281d13848e38
Nick Tran <nick.tran@sparkventures.co.nz>
2017-02-20 09:35:54 +1300

    Annotated FileReader

--- regression/README.md
+++ regression/README.md
@@ -2,3 +2,4 @@
 How to run tests
 1) Open Cucumber Runner
 2) Set VM options = -Dselenide.browser=chrome -Dwebdriver.chrome.driver=/<path to chrome driver>/chromedriver
+e.g. -Dselenide.browser=chrome -Dwebdriver.chrome.driver=/Users/nicktran/chromedriver
--- regression/src/main/java/nz/co/automation/regression/io/FileReader.java
+++ regression/src/main/java/nz/co/automation/regression/io/FileReader.java
@@ -1,7 +1,10 @@
 package nz.co.automation.regression.io;
 
+import org.springframework.stereotype.Component;
+
 import java.io.InputStream;
 
+@Component
 public class FileReader {
     public InputStream read(String path) {
         return Thread.currentThread().getContextClassLoader().getResourceAsStream(path);
