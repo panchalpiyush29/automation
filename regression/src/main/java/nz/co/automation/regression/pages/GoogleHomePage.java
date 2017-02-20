bc4674cb3114c12dded5caea159b32244a317daf
Nick Tran <nick.tran@sparkventures.co.nz>
2017-02-21 09:12:18 +1300

    Implemented GoogleHomePage

--- regression/src/main/java/nz/co/automation/regression/pages/GoogleHomePage.java
+++ regression/src/main/java/nz/co/automation/regression/pages/GoogleHomePage.java
@@ -1,8 +1,18 @@
 package nz.co.automation.regression.pages;
 
+import com.codeborne.selenide.SelenideElement;
 import nz.co.automation.regression.domain.Query;
+import org.springframework.stereotype.Component;
 
+import static com.codeborne.selenide.Selenide.$;
+
+@Component
 public class GoogleHomePage {
+    private final String ID_SEARCH = "#lst-ib";
+
     public void search(Query query) {
+        final SelenideElement searchBox = $(ID_SEARCH);
+        searchBox.sendKeys(query.getQuery());
+        searchBox.pressEnter();
     }
 }
