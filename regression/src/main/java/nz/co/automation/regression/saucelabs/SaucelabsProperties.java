d916243b9449b467887173e1eff2bf62d17d2027
Nick Tran <nick.tran@sparkventures.co.nz>
2017-03-21 12:35:16 +0700

    Removed unused import - Always COMMAND + ALT + L to clean up after finishing coding on a file guys!

--- regression/src/main/java/nz/co/automation/regression/saucelabs/SaucelabsProperties.java
+++ regression/src/main/java/nz/co/automation/regression/saucelabs/SaucelabsProperties.java
@@ -1,7 +1,6 @@
 package nz.co.automation.regression.saucelabs;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
 
 @Component
