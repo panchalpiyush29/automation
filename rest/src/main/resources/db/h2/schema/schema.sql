bce7d3d68f5cff45e6cae0a418c233e06573ffb9
Nick Tran <nick.tran@sparkventures.co.nz>
2017-08-20 22:16:27 +1200

    Added h2

---
+++ rest/src/main/resources/db/h2/schema/schema.sql
@@ -0,0 +1,6 @@
+create table dogs (
+  id int not null auto_increment,
+  name varchar(200) not null,
+  age int not null,
+  primary key (id)
+);
---
+++ rest/src/main/resources/db/h2/seed/data.sql
@@ -0,0 +1,5 @@
+insert into dogs(name, age)
+    values('Chihuahua', 2);
+
+insert into dogs(name, age)
+values('Phu Quoc', 3);
